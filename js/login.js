ed4be5cb833106b3065e348dad015935be0da421
João Lucas Alberto Muniz <joaolucas.power@gmail.com>
2022-06-03 01:54:27 -0300

    Implementação de notificação

--- js/login.js
+++ js/login.js
@@ -1,3 +1,20 @@
+toastr.options = {
+    "closeButton": true,
+    "debug": false,
+    "newestOnTop": false,
+    "progressBar": false,
+    "positionClass": "toast-bottom-center",
+    "preventDuplicates": false,
+    "onclick": null,
+    "showDuration": "300",
+    "hideDuration": "1000",
+    "timeOut": "5000",
+    "extendedTimeOut": "1000",
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut"
+}
 const entrarBtn = document.getElementById("entrarBtn");
 const senhaInput = document.getElementById("InputPassword");
 
@@ -7,12 +24,14 @@ entrarBtn.addEventListener("click", function() {
     const re = /\S+@\S+\.\S+/;
     if (!re.test(email)) {
         $("#emailLogin").addClass("is-invalid");
+        toastr.error('Email inválido')
     } else {
         $("#emailLogin").removeClass("is-invalid");
         $("#emailLogin").addClass('is-valid');
     }
     if (senha === "") {
         $("#InputPassword").addClass("is-invalid");
+        toastr.error('Senha inválida')
     } else {
         $("#InputPassword").removeClass("is-invalid");
         $("#InputPassword").addClass('is-valid');
