8504d44f86785a837f20263114f1ac048507b8de
gabisegger <gabi.segger@gmail.com>
2022-06-03 11:17:11 -0300

    feat: password validation

--- js/validacao-campos.js
+++ js/validacao-campos.js
@@ -8,14 +8,26 @@ function validateEmail(email) {
 document.getElementById("form").addEventListener("submit", function(e) {
 
     const email = document.getElementById("email").value;
+    const emailError = document.getElementById("email")
 
     const validaEmail = validateEmail(email);
 
     if (validaEmail == false){
         e.preventDefault()
         const errorEmail = document.querySelector('.error-email')
-        errorEmail.textContent('Digite um e-mail válido!')
+        errorEmail.textContent = 'Digite um e-mail válido!'
+        emailError.style.border = '2px solid red'
     }
     
+    //Valida senha
+    const password = document.getElementById("password")
+    const confirmPassword = document.getElementById("confirm_password");
+    const errorSenha = document.querySelector('.error-senha')
+    
+    if(password.value != confirmPassword.value) {
+        errorSenha.textContent = 'Senhas diferentes!'
+        password.style.border = '2px solid red'
+        confirmPassword.style.border = '2px solid red'
+    }
 
 })
