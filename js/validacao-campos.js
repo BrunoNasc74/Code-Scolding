3d7591ad24b09082cb0565ec4fbe4609ab7aecb3
BrunoNasc74 <bruno.hpn74@gmail.com>
2022-06-07 18:45:37 -0300

    correção de validação

--- js/validacao-campos.js
+++ js/validacao-campos.js
@@ -1,112 +1,120 @@
 // Function Valida E-mail
 function validateEmail(email) {
-    var re = /\S+@\S+\.\S+/;
-    return re.test(email);
+  var re = /\S+@\S+\.\S+/;
+  return re.test(email);
 }
 
 // Function Valida RG
 function validateRg(rg) {
-    var re = /(^\d{1,2}).?(\d{3}).?(\d{3})-?(\d{1}|X|x$)/;
-    return re.test(rg);
+  var re = /(^\d{1,2}).?(\d{3}).?(\d{3})-?(\d{1}|X|x$)/;
+  return re.test(rg);
 }
 
 // Evento de submit do form
-document.getElementById("form").addEventListener("submit", function(e) {
-    e.preventDefault()
-    limparErros();
-    //Valida E-mail
-    const email = document.getElementById("email").value;
-    const emailError = document.getElementById("email")
+document.getElementById('form').addEventListener('submit', function (e) {
+  e.preventDefault();
+  limparErros();
+  //Valida E-mail
+  const email = document.getElementById('email').value;
+  const emailError = document.getElementById('email');
 
-    const validaEmail = validateEmail(email);
+  const validaEmail = validateEmail(email);
 
-    const errorEmail = document.querySelector('.error-email')
+  const errorEmail = document.querySelector('.error-email');
 
-    console.log(validaEmail, email.length);
+  console.log(validaEmail, email.length);
 
-    if (validaEmail == false && email.length > 0){
-        errorEmail.textContent = 'Digite um e-mail válido!'
-        emailError.style.border = '2px solid red'
-    } else {
-        errorEmail.textContent = ''
-        emailError.style.border = 'none'
-    }
-    
-    //Valida senha
-    const password = document.getElementById("password")
-    const confirmPassword = document.getElementById("confirm_password");
-    const errorSenha = document.querySelector('.error-senha')
-    
-    if(password.value != confirmPassword.value) {
-        errorSenha.textContent = 'Senhas diferentes!'
-        password.style.border = '2px solid red'
-        confirmPassword.style.border = '2px solid red'
-    } else{
-        errorSenha.textContent = ''
-        password.style.border = 'none'
-        confirmPassword.style.border = 'none'
-    }
+  if (validaEmail == false && email.length > 0) {
+    errorEmail.textContent = 'Digite um e-mail válido!';
+    emailError.style.border = '2px solid red';
+  } else {
+    errorEmail.textContent = '';
+    emailError.style.border = 'none';
+  }
 
-    //Valida RG
-    const rg = document.getElementById("rg").value;
-    const errorRg = document.getElementById("rg");
-    const validaRg = validateRg(rg);
-    const rgError = document.querySelector(".error-rg")
+  //Valida senha
+  const password = document.getElementById('password');
+  const confirmPassword = document.getElementById('confirm_password');
+  const errorSenha = document.querySelector('.error-senha');
 
-    if (validaRg == false && rg.length > 0){
-        rgError.textContent = 'Digite um RG válido!'
-        errorRg.style.border = '2px solid red'
-    } else {
-        rgError.textContent = ''
-        errorRg.style.border = 'none'
-    }
-    
-    const hasError = validadeFields();
-    const success = document.querySelector(".success")
+  if (password.value != confirmPassword.value) {
+    errorSenha.textContent = 'Senhas diferentes!';
+    password.style.border = '2px solid red';
+    confirmPassword.style.border = '2px solid red';
+  } else {
+    errorSenha.textContent = '';
+    password.style.border = 'none';
+    confirmPassword.style.border = 'none';
+  }
 
-    if (!hasError && validaEmail && password.value === confirmPassword.value && validaRg) {
-        success.textContent = 'Cadastro realizado com sucesso!'
-        success.style.color = '#38ce36!important'
-        limparForm()
-    }
+  //Valida RG
+  const rg = document.getElementById('rg').value;
+  const errorRg = document.getElementById('rg');
+  const validaRg = validateRg(rg);
+  const rgError = document.querySelector('.error-rg');
 
-})
+  if (validaRg == false && rg.length > 0) {
+    rgError.textContent = 'Digite um RG válido!';
+    errorRg.style.border = '2px solid red';
+  } else {
+    rgError.textContent = '';
+    errorRg.style.border = 'none';
+  }
+
+  const hasError = validadeFields();
+  const success = document.querySelector('.success');
+  const erro = document.querySelector('.error');
+
+  if (
+    !hasError &&
+    validaEmail &&
+    password.value === confirmPassword.value &&
+    validaRg
+  ) {
+    erro.remove();
+    success.textContent = 'Cadastro realizado com sucesso!';
+    success.style.color = '#38ce36!important';
+    limparForm();
+    setTimeout(() => {
+      window.location.href = '../index.html';
+    }, 1000);
+  }
+});
 
 //Valida campos não preenchidos
-function validadeFields(){
-    const fields = document.querySelectorAll('input:not(#complemento)');
-    const erro = document.querySelector('.error');
-    let hasError = false;
-    
-    for(let i = 0; i < fields.length; i++) {
-        
-        if(fields[i].value.length <= 0){
-            fields[i].style.border = '2px solid red'
-            erro.textContent = 'Preencha os campos obrigatórios!'
-            hasError = true;
-        } 
-    }
+function validadeFields() {
+  const fields = document.querySelectorAll('input:not(#complemento)');
+  const erro = document.querySelector('.error');
+  let hasError = false;
 
-    return hasError;
+  for (let i = 0; i < fields.length; i++) {
+    if (fields[i].value.length <= 0) {
+      fields[i].style.border = '2px solid red';
+      erro.textContent = 'Preencha os campos obrigatórios!';
+      hasError = true;
+    }
+  }
+
+  return hasError;
 }
 
-function limparForm(){
-    const inputs = document.querySelectorAll('input');
+function limparForm() {
+  const inputs = document.querySelectorAll('input');
 
-    for(let i = 0; i < inputs.length; i++) {
-        inputs[i].value = '';
-    }
+  for (let i = 0; i < inputs.length; i++) {
+    inputs[i].value = '';
+  }
 }
 
 function limparErros() {
-    const inputs = document.querySelectorAll('input:not(#cep)');
+  const inputs = document.querySelectorAll('input:not(#cep)');
 
-    for(let i = 0; i < inputs.length; i++) {
-        if(inputs[i].value.length > 0) {
-            inputs[i].style.border = 'none';
-            if (inputs[i].parentElement.querySelector('span')) {
-                inputs[i].parentElement.querySelector('span').textContent = '';
-            }
-        }
+  for (let i = 0; i < inputs.length; i++) {
+    if (inputs[i].value.length > 0) {
+      inputs[i].style.border = 'none';
+      if (inputs[i].parentElement.querySelector('span')) {
+        inputs[i].parentElement.querySelector('span').textContent = '';
+      }
     }
+  }
 }
